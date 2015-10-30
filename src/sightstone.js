e5fc37ac95f45a940e63ed3553fdffa150454eb7
Boyang Niu <boyang.niu@gmail.com>
2015-10-30 02:17:48 -0400

    Add redis, fix response method

--- src/sightstone.js
+++ src/sightstone.js
@@ -1,24 +1,30 @@
 var req = require('request'),
     Q = require('q'),
+    redis = require('redis'),
     assign = require('object-assign');
 
 class Sightstone {
-  constructor(api_key, region){
+  constructor(api_key, region, redis_options = undefined){
+    if(typeof(redis_options) === 'object'){
+      this.redis = redis.createClient(redis_options);
+    }
     this.api_key = api_key;
     this.region = region;
     this.base_params = {api_key: this.api_key};
     this.base_url = "https://na.api.pvp.net/api/lol";
   }
-  static validate_and_respond(url, params){
-    let defer = Q.defer();
-    let request = {url: url, qs: params};
+  cacheResponse(url, response){
 
+  }
+  validateAndRespond(url, params){
+    let defer = Q.defer();
     if(typeof(params) === "object"){
-      assign(params, this.base_params, params);  
+      assign(params, this.base_params, params);
     } else {
       defer.reject(new Error("Cannot use non-object params"));
       return defer.promise;
     }
+    let request = {url: url, qs: params};
     req(request, function(err, resp, body){
       if(resp.statusCode != 200){
         defer.reject(new Error(body));
@@ -29,45 +35,45 @@ class Sightstone {
     return defer.promise;
   }
   champions(params={}){
-    return Sightstone.validate_and_respond(
+    return this.validateAndRespond(
       `${this.base_url}/${this.region}/v1.2/champion`, params);
   }
   recentGames(summoner_id, params={}){
-    return Sightstone.validate_and_respond(
+    return this.validateAndRespond(
       `${this.base_url}/${this.region}/v1.3/game/by-summoner/${summoner_id}/recent`, params);
   }
   league(summoner_ids, params={}){
     if(typeof(summoner_ids) === "object"){
       summoner_ids = summoner_ids.join(",");
     }
-    return Sightstone.validate_and_respond(
+    return this.validateAndRespond(
       `${this.base_url}/${this.region}/v2.5/league/by-summoner/${summoner_ids}`, params);
   }
   matchlist(summoner_id, params={}){
-    return Sightstone.validate_and_respond(
+    return this.validateAndRespond(
       `${this.base_url}/${this.region}/v2.2/matchlist/by-summoner/${summoner_id}`, params);
   }
   static_data(type, params={}){
-    return Sightstone.validate_and_respond(
+    return this.validateAndRespond(
       `${this.base_url}/static-data/${this.region}/v1.2/${type}`, params);
   }
   match(match_id, params={}){
-    return Sightstone.validate_and_respond(
+    return this.validateAndRespond(
       `${this.base_url}/${this.region}/v2.2/match/${match_id}`, params);
   }
   summoner(names, params={}){
     if(typeof(names) === "object"){
       names = names.join(",");
     }
-    return Sightstone.validate_and_respond(
+    return this.validateAndRespond(
       `${this.base_url}/${this.region}/v1.4/summoner/by-name/${names}`, params);
   }
   stats_ranked(summoner_id, params={}){
-    return Sightstone.validate_and_respond(
+    return this.validateAndRespond(
       `${this.base_url}/${this.region}/v1.3/stats/by-summoner/${summoner_id}/ranked`, params);    
   }
   stats_summary(summoner_id, params={}){
-    return Sightstone.validate_and_respond(
+    return this.validateAndRespond(
       `${this.base_url}/${this.region}/v1.3/stats/by-summoner/${summoner_id}/summary`, params);    
   }
 }
--- test/sightstone.js
+++ test/sightstone.js
@@ -7,9 +7,4 @@ describe('#constructor', function(){
 		(ss.api_key).should.equal("test");
 		(ss.region).should.equal("na");
 	});
-	it("makes an http request to the riot api", function(){
-		var ss = new Sightstone("092ae6cc-f0a4-4dea-ae7b-06d5af385c7f", "na");
-		var c = ss.champions();
-		console.log(typeof(c));
-	});
 });
